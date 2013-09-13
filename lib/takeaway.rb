519d985feb6d55cda17db115fc2bac7a95449661
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-13 19:08:16 +0100

    Completed text message confirmation using Twilio API

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,12 +1,25 @@
+require 'bundler/setup'
+require 'twilio-ruby'
+
 class Takeaway
   def dishes
-    { :tonkatsu => 6, :chashumen => 5, :teriyaki => 4, :edamame => 2 }
+    { :tonkatsu => 6,
+      :chashumen => 5,
+      :teriyaki => 4,
+      :edamame => 2 }
   end
 
   def place_order(dishes_and_quantities, order_total)
     actual_price = check_price_of(dishes_and_quantities)
     raise_order_total_error if actual_price != order_total
-    true
+    send_order_confirmation
+  end
+
+  def send_order_confirmation
+    one_hour_from_now = (Time.now + 60 * 60).strftime("%H:%M")
+    order_confirmation = "Thank you! Your order was successfully " +
+     "placed and will be delivered before #{one_hour_from_now}"
+    send_text(order_confirmation)
   end
 
   def check_price_of(dishes_and_quantities)
@@ -17,5 +30,18 @@ class Takeaway
 
   def raise_order_total_error
     raise ArgumentError, 'Total is wrong. Please place the order again.'
+  end
+
+  def twilio_client
+    account_sid = 'ACfe1de759bee1087682f75412ba2d84e9'
+    auth_token = 'a36642a3ad953064856e1d0664d1e51f'
+    @twilio_client ||= Twilio::REST::Client.new account_sid, auth_token
+  end
+
+  def send_text(message)
+    twilio_client.account.sms.messages.create(
+      :from => '+441924950381',
+      :to => '+447881957811',
+      :body => message)
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -9,6 +9,7 @@ describe Takeaway do
   end
 
   it 'should allow an order to be placed' do
+    expect(takeaway).to receive(:send_text).and_return true
     expect(takeaway.place_order({ :tonkatsu => 1, :edamame => 2 }, 10)).to be_true
   end
 
@@ -18,5 +19,45 @@ describe Takeaway do
     }.to raise_error ArgumentError, 'Total is wrong. Please place the order again.'
   end
 
-  it 'should send an order confirmation by text if an order is placed correctly'
+  it 'should start a new Twilio API client to send text messages' do
+    twilio_client = double :TwilioRESTClient
+    expect(Twilio::REST::Client).to receive(:new).and_return twilio_client
+    expect(takeaway.twilio_client).to be twilio_client
+  end
+
+  it 'should use the existing Twilio API client if available' do
+    twilio_client = double :TwilioRESTClient
+    expect(Twilio::REST::Client).to receive(:new).and_return twilio_client
+    takeaway.twilio_client
+    expect(takeaway.twilio_client). to be twilio_client
+  end
+
+  it 'should be able to send text messages' do
+    message = "Thank you!"
+    twilio_client = double :TwilioRESTClient
+    account = double :account
+    sms = double :sms
+    messages  = double :messages
+    expect(takeaway).to receive(:twilio_client).and_return twilio_client
+    expect(twilio_client).to receive(:account).and_return account
+    expect(account).to receive(:sms).and_return sms
+    expect(sms).to receive(:messages).and_return messages
+    expect(messages).to receive(:create) do |message_hash|
+      expect(message_hash[:body]).to eq message
+    end
+    takeaway.send_text(message)
+  end
+
+  it 'should send an order confirmation by text' do
+    expect(takeaway).to receive(:send_text).and_return true
+    takeaway.place_order({ :tonkatsu => 1, :edamame => 2 }, 10)
+  end
+
+  it 'should include the delivery time of one hour from now in the text' do
+    one_hour_from_now = (Time.now + 60 * 60).strftime("%H:%M")
+    expect(takeaway).to receive(:send_text) do |message|
+      expect(message).to include one_hour_from_now
+    end
+    takeaway.place_order({ :tonkatsu => 1, :edamame => 2 }, 10)
+  end
 end
