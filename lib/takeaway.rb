e2ad18d6fdb5b743e524a0ac5986831a7fd10411
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-13 17:48:47 +0100

    Started takeaway spec

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,21 @@
 class Takeaway
+  def dishes
+    { :tonkatsu => 6, :chashumen => 5, :teriyaki => 4, :edamame => 2 }
+  end
+
+  def place_order(dishes_and_quantities, order_total)
+    actual_price = check_price_of(dishes_and_quantities)
+    raise_order_total_error if actual_price != order_total
+    true
+  end
+
+  def check_price_of(dishes_and_quantities)
+    dishes_and_quantities.inject(0) do |price, (dish, quantity)|
+      price += dishes[dish] * quantity
+    end
+  end
+
+  def raise_order_total_error
+    raise ArgumentError, 'Total is wrong. Please place the order again.'
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,22 @@
 require 'takeaway'
 
 describe Takeaway do
-  
+  let(:takeaway) { Takeaway.new }
+
+  it 'should have a list of dishes' do
+    dishes = { :tonkatsu => 6, :chashumen => 5, :teriyaki => 4, :edamame => 2 }
+    expect(takeaway.dishes).to eq dishes
+  end
+
+  it 'should allow an order to be placed' do
+    expect(takeaway.place_order({ :tonkatsu => 1, :edamame => 2 }, 10)).to be_true
+  end
+
+  it 'should raise an error if an order is placed with the wrong total' do
+    expect { 
+      takeaway.place_order({ :edamame => 2 }, 1)
+    }.to raise_error ArgumentError, 'Total is wrong. Please place the order again.'
+  end
+
+  it 'should send an order confirmation by text if an order is placed correctly'
 end
